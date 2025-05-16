3becc0006003f6165f1d77591b2ee74655875c92
Michael Spyratos <m.spyratos@gmail.com>
2025-05-16 18:25:35 -0400

    Setup mongo memory server

--- src/paginationQuery.test.ts
+++ src/paginationQuery.test.ts
@@ -1,8 +1,37 @@
-import {describe, it, before} from 'node:test';
+import {describe, it, before, after} from 'node:test';
 import assert from 'node:assert';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import {Collection, Db, MongoClient} from 'mongodb';
 
 describe('tests',  () => {
-    before(() => console.log('about to run some test'));
+    let con: MongoClient;
+    let mongoServer: MongoMemoryServer;
+    let db: Db;
+    let collection: Collection;
+
+    before(async () => {
+        mongoServer = await MongoMemoryServer.create();
+        con = await MongoClient.connect(mongoServer.getUri(), {});
+        db = con.db(mongoServer.instanceInfo!.dbName);
+        collection = db.collection('test');
+    });
+
+    after(async () => {
+        if (con) {
+            await con.close();
+        }
+        if (mongoServer) {
+            await mongoServer.stop();
+        }
+    });
+
+    it('successfully sets & gets information from the database', async () => {
+        assert.notEqual(db, undefined);
+        const result = await collection.insertMany([{ a: 1 }, { b: 1 }]);
+        assert.strictEqual(result.insertedCount, 2);
+        assert.equal(await collection.countDocuments({}), 2);
+    });
+
     it('is a subtest', () => {
         assert.ok('some relevant assertion here');
     });
