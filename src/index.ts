e884916c97f622d002d5c514eabf03cd10a71aab
Michael Spyratos <m.spyratos@gmail.com>
2025-05-16 14:11:03 -0400

    Native TS setup and build

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import {isMike} from './paginationQuery.ts';
+
+console.log(isMike('Mike'));
+console.log(isMike('John'));
---
+++ src/paginationQuery.ts
@@ -0,0 +1,9 @@
+
+export function isMike(name: string): boolean {
+    switch (name) {
+        case 'Mike':
+            return true;
+        default:
+            return false;
+    }
+}
